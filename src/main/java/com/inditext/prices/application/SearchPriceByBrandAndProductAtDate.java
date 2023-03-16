da9144145cf9ef13c90baf715c66f2bb4045f7e2
David Garcia Latorre <david.garcia@eydevelopment.com>
2023-03-16 11:47:34 +0100

    WIP

--- src/main/java/com/inditext/prices/application/SearchPriceByBrandAndProductAtDate.java
+++ src/main/java/com/inditext/prices/application/SearchPriceByBrandAndProductAtDate.java
@@ -1,6 +1,15 @@
 package com.inditext.prices.application;
 
+import com.inditext.prices.domain.PriceRepository;
+
 public class SearchPriceByBrandAndProductAtDate {
+
+    private final PriceRepository repository;
+
+    public SearchPriceByBrandAndProductAtDate(PriceRepository repository) {
+        this.repository = repository;
+    }
+
     public SearchPriceByBrandAndProductAtDateResponse execute(SearchPriceByBrandAndProductAtDateRequest request) {
         return null;
     }
--- src/test/java/com/inditext/prices/application/SearchPriceByBrandAndProductAtDateTest.java
+++ src/test/java/com/inditext/prices/application/SearchPriceByBrandAndProductAtDateTest.java
@@ -1,27 +1,52 @@
 package com.inditext.prices.application;
 
+import com.inditext.prices.domain.PriceRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 
+@ExtendWith(MockitoExtension.class)
 class SearchPriceByBrandAndProductAtDateTest {
+    private static final int BRAND_ID = 1;
+    private static final int PRODUCT_ID = 35455;
+    private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private static final Date DATE = toDate("2020-06-15 00:00:00");
 
-    SearchPriceByBrandAndProductAtDate sut;
+    @Mock
+    private PriceRepository repository;
+
+    private SearchPriceByBrandAndProductAtDate sut;
 
     @BeforeEach
     void setUp() {
-        sut = new SearchPriceByBrandAndProductAtDate();
+        sut = new SearchPriceByBrandAndProductAtDate(repository);
     }
 
     @Test
     public void should_return_valid_price() {
         SearchPriceByBrandAndProductAtDateRequest request = new SearchPriceByBrandAndProductAtDateRequest();
         SearchPriceByBrandAndProductAtDateResponse expected = new SearchPriceByBrandAndProductAtDateResponse();
+        given(repository.searchByBrandProductAtDate(BRAND_ID, PRODUCT_ID, DATE));
 
         SearchPriceByBrandAndProductAtDateResponse result = sut.execute(request);
 
-        assertEquals( expected, result );
+        assertEquals(expected, result);
     }
 
+    private static Date toDate(String date) {
+        try {
+            return SIMPLE_DATE_FORMAT.parse(date);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
