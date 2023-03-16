7288ab6d9bce8898b785b872186ffe787946fd8d
David Garcia Latorre <david.garcia@eydevelopment.com>
2023-03-16 13:11:23 +0100

    Testing controller responses. Happy and not happy path.

---
+++ src/test/java/com/inditext/prices/infra/api/PriceControllerTest.java
@@ -0,0 +1,78 @@
+package com.inditext.prices.infra.api;
+
+import com.inditext.prices.application.SearchPriceByBrandAndProductAtDate;
+import com.inditext.prices.application.SearchPriceByBrandAndProductAtDateRequest;
+import com.inditext.prices.application.SearchPriceByBrandAndProductAtDateResponse;
+import com.inditext.prices.domain.exception.InvalidDateException;
+import com.inditext.prices.domain.exception.PriceNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class PriceControllerTest {
+    private static final int BRAND_ID = 1;
+    private static final int PRODUCT_ID = 35455;
+    private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private static final String START_DATE = "2020-06-15 00:00:00";
+    private static final String DATE_AS_STRING = START_DATE;
+    private static final String BAD_DATE = "INVALID_DATE_VALUE";
+    private static final int PRICE_ID = 3;
+    private static final String END_DATE = "2020-06-15 11:00:00";
+    private static final double PRICE = 30.50;
+    private static final SearchPriceByBrandAndProductAtDateRequest REQUEST = new SearchPriceByBrandAndProductAtDateRequest(BRAND_ID, PRODUCT_ID, DATE_AS_STRING);
+    private static final SearchPriceByBrandAndProductAtDateRequest INVALID_REQUEST = new SearchPriceByBrandAndProductAtDateRequest(BRAND_ID, PRODUCT_ID, BAD_DATE);
+    private static final SearchPriceByBrandAndProductAtDateResponse RESPONSE = new SearchPriceByBrandAndProductAtDateResponse(
+            BRAND_ID, PRODUCT_ID, PRICE_ID, START_DATE, END_DATE, PRICE
+    );
+    @Mock
+    private SearchPriceByBrandAndProductAtDate searchPriceByBrandAndProductAtDate;
+
+    private PriceController sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new PriceController(searchPriceByBrandAndProductAtDate);
+    }
+
+    @Test
+    public void should_return_valid_price_when_valid_request() {
+        given(searchPriceByBrandAndProductAtDate.execute(REQUEST)).willReturn(RESPONSE);
+        ResponseEntity<SearchPriceByBrandAndProductAtDateResponse> expected = ResponseEntity.ok().body(RESPONSE);
+
+        ResponseEntity<SearchPriceByBrandAndProductAtDateResponse> response = sut.searchPriceByBrandAndProductAtDate(BRAND_ID, PRODUCT_ID, DATE_AS_STRING);
+
+        assertEquals(expected, response);
+    }
+
+    @Test
+    public void should_return_empty_response_if_price_not_exist() {
+        given(searchPriceByBrandAndProductAtDate.execute(REQUEST)).willThrow(PriceNotFoundException.class);
+        ResponseEntity<SearchPriceByBrandAndProductAtDateResponse> expected = ResponseEntity.noContent().build();
+
+        ResponseEntity<SearchPriceByBrandAndProductAtDateResponse> response = sut.searchPriceByBrandAndProductAtDate(BRAND_ID, PRODUCT_ID, DATE_AS_STRING);
+
+        assertEquals(expected, response);
+    }
+
+    @Test
+    public void should_return_bad_request_if_dates_are_not_parseable() {
+        given(searchPriceByBrandAndProductAtDate.execute(INVALID_REQUEST)).willThrow(InvalidDateException.class);
+        ResponseEntity<SearchPriceByBrandAndProductAtDateResponse> expected = ResponseEntity.badRequest().build();
+
+        ResponseEntity<SearchPriceByBrandAndProductAtDateResponse> response = sut.searchPriceByBrandAndProductAtDate(BRAND_ID, PRODUCT_ID, BAD_DATE);
+
+        assertEquals(expected, response);
+    }
+
+}
