862a5c7b5f5f2eb51b1a4cca2f7e42e33ee26421
David Garcia Latorre <david.garcia@eydevelopment.com>
2023-03-16 13:22:11 +0100

    Simplify with controller advice

---
+++ src/main/java/com/inditext/prices/infra/config/Exception.java
@@ -0,0 +1,20 @@
+package com.inditext.prices.infra.config;
+
+import com.inditext.prices.domain.exception.PriceNotFoundException;
+import com.inditext.prices.domain.exception.InvalidDateException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class Exception {
+    @ExceptionHandler(PriceNotFoundException.class)
+    public ResponseEntity priceNotFound(HttpServletRequest req, Exception e) {
+        return ResponseEntity.noContent().build();
+    }
+    @ExceptionHandler(InvalidDateException.class)
+    public ResponseEntity invalidDate(HttpServletRequest req, Exception e) {
+        return ResponseEntity.badRequest().build();
+    }
+}
